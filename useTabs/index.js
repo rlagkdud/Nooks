3cdeb183fea9a636849317ab7f2cc218301b387b
rlagkdud <ha0936861@gmail.com>
2021-02-02 14:44:14 +0900

    useTabs

---
+++ useTabs/index.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+
+const content = [
+  {
+    tab: "Section 1",
+    content: "I'm the content of Section 1"
+  },
+  {
+    tab: "Section 2",
+    content: "I'm the content of Section 2"
+  }
+];
+
+export default function App() {
+    const { currentItem , changeItem} = useTabs(0, content);
+    return (
+      <div className="App">
+        {content.map((section,index) => (
+          <button onClick={()=>changeItem(index)}>{section.tab}</button>
+        ))}
+        <div>{currentItem.content}</div>
+      </div>
+    );
+  }
+  
+  const rootElement = document.getElementById("root");
+  ReactDOM.render(<App />, rootElement);
+  
